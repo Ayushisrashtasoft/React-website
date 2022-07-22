9a52f719d25383f31677d0445eab4f8e85d7c177
Ayushisrashtasoft <ayushipatel.srashtasoft@gmail.com>
2022-07-22 10:51:42 +0530

    final commit

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -63,9 +63,7 @@ function Navbar() {
                 <div className="navbar-close-button">
                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     X
-
-
-                  </button>
+                    </button>
                 </div>
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
